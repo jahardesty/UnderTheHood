2e33a7887472c656f745867c40da8a38d2832779
Jeff Hardesty <jahardesty7@gmail.com>
2026-08-25 12:31:47 -0400

    Initial Commit

---
+++ UnderTheHood/UnderTheHoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderTheHoodApp.swift
+//  UnderTheHood
+//
+//  Created by Jeff on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct UnderTheHoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
